41acd58b3f6a520158ea758855bbfeee54974712
chang <jkc26@njit.edu>
2019-03-23 23:36:18 -0400

    Fix: Posts in progress

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: chang
+ * Date: 3/23/2019
+ * Time: 11:07 PM
+ */
+use Faker\Generator as Faker;
+
+$factory -> define(\App\Post::class, function(Faker $faker){
+    return[
+
+];
+});
